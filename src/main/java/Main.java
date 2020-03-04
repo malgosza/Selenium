35f94bb2dd697d6ef6c108abf2f0b6e88805a9d6
malgosza <malgosza.niewiadomska@gmail.com>
2020-03-04 13:25:54 +0100

    Navigate vs get

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,12 +6,18 @@ import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
-        String baseURL="https://learn.letskodeit.com";
+        String baseURL = "https://facebook.com";
         WebDriver driver;
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\Kamil\\Desktop\\code\\Selenium\\chromedriver_win32\\chromedriver.exe");
-        driver=new ChromeDriver();
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get(baseURL);
+        String title = driver.getTitle();
+        System.out.println("Title of the page is: " + title);
+        String currentURL = driver.getCurrentUrl();
+        System.out.println("Current of URL is: " + currentURL);
+        String pageSource = driver.getPageSource();
+        System.out.println("Page of Source is: " + pageSource);
     }
 }
