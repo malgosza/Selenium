d6d60366c7449728384a81fbcd57a7350153bb4d
Gosia <malgosza.niewiadomska@gmail.com>
2020-03-12 10:15:24 +0100

    ScreenShots part 2

--- src/test/java/ScreenShots.java
+++ src/test/java/ScreenShots.java
@@ -1,10 +1,14 @@
+import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 public class ScreenShots {
@@ -14,7 +18,7 @@ public class ScreenShots {
     @Before
     public void setUp() throws Exception {
         System.setProperty("webdriver.chrome.driver",
-                "C:\\Users\\Kamil\\Desktop\\code\\Selenium\\chromedriver_win32\\chromedriver.exe");
+                "C:\\Users\\User\\Desktop\\chromedriver_win32\\chromedriver.exe");
 
         driver = new ChromeDriver();
         baseURL = "https://facebook.com";
@@ -26,11 +30,24 @@ public class ScreenShots {
     @Test
     public void testScreenShots() throws Exception {
         driver.get(baseURL);
-        driver.findElement(By.id("u_0_2")).click();
+        driver.findElement(By.id("u_0_b")).click();
     }
 
     @After
     public void tearDown() throws Exception {
-        driver.quit();
+        String filename = getRandomString(10) + ".png";
+        String directory = System.getProperty("user.dir") + "//screenshots//";
+        File sourceFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(sourceFile, new File(directory + filename));
+        driver.quit();    }
+
+    public static String getRandomString(int length) {
+        StringBuilder sb = new StringBuilder();
+        String characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        for (int i = 0; i < length; i++) {
+            int index = (int)(Math.random() * characters.length());
+            sb.append(characters.charAt(index));
+        }
+        return sb.toString();
     }
 }
